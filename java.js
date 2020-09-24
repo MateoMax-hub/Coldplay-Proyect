745b2699f95974d756a68d800fe67b48299661e6
Mateo-Max <max.mateo.02@gmail.com>
2020-09-24 19:40:00 -0300

    add java para boton navbar y some img

---
+++ java.js
@@ -0,0 +1,7 @@
+function navbar() {
+    if (document.getElementById('navbar').style.display == 'none'){
+    document.getElementById('navbar').style.display = 'block'
+    } else {
+        document.getElementById('navbar').style.display = 'none'
+    }
+}
